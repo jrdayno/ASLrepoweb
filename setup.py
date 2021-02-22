f14c2ffe39fd0984041cfb0980138ced76d6b5f4
Dunno <jdayno@ryerson.ca>
2021-02-22 13:41:14 -0500

    Upload Cookiecutter Template

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name='src',
+    packages=find_packages(),
+    version='0.1.0',
+    description='Use MLFlow to record models made to predict/recognize the ASL letter in given pictures(jpg)',
+    author='Jayden Dayno',
+    license='',
+)
